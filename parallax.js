7cbd43c79e57383f630186eb2ffd3feac99a7f91
DorKatzir <DorKatzir@users.noreply.github.com>
2020-04-04 01:43:29 +0300

    adding the js scroll code

--- parallax.js
+++ parallax.js
@@ -1,1 +1,15 @@
-console.log("in");
+let bg = document.getElementById('#bg');
+let moon = document.getElementById('#moon');
+let mountain = document.getElementById('#mountain');
+let road = document.getElementById('#road');
+let text = document.getElementById('#text');
+
+window.addEventListener('scroll', function(){
+    const value = window.scrollY;
+    // console.log(value);
+    this.bg.style.top = value * 0.5 + "px";
+    this.moon.style.left = -value * 0.5 + "px";
+    this.mountain.style.top = -value * 0.15 + "px";
+    this.road.style.top = value * 0.15 + "px";
+    this.text.style.top = value * 1 + "px";
+});
